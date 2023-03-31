7efa23e3c6ea53097b32fd142e34aa97ebc85e83
gerardogarnica <gerardo.garnica@gmail.com>
2023-03-30 21:26:40 -0500

    Create auth module.

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './pages/login/login.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+    title: 'Login'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
+// Routing
 import { AuthRoutingModule } from './auth-routing.module';
 
+// Components
+
+// Pages
+import { LoginComponent } from './pages/login/login.component';
+
+// Modules
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    SharedModule
   ]
 })
 export class AuthModule { }
