6bfd27ac917e2ec17a2ae8ca68fdfe14c00cd271
gerardogarnica <gerardo.garnica@gmail.com>
2023-07-20 21:22:05 -0500

    Add redirection guard.

---
+++ src/app/guards/redirect.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+
+// Services
+import { TokenService, TokenType } from '@services/token.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RedirectGuard implements CanActivate {
+  constructor(
+    private tokenService: TokenService,
+    private router: Router
+  ) { }
+
+  canActivate(): boolean {
+    if (this.tokenService.isValidToken(TokenType.Refresh)) {
+      this.router.navigate(['/home']);
+    }
+    return true;
+  }
+  
+}
