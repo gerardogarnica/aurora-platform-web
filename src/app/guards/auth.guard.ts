f9362f4fcdd805e125daa38ae98bcd42164a5842
gerardogarnica <gerardo.garnica@gmail.com>
2023-07-20 21:19:37 -0500

    Add authorization guard.

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+
+// Services
+import { TokenService, TokenType } from '@services/token.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private tokenService: TokenService,
+    private router: Router
+  ) { }
+
+  canActivate(): boolean {
+    if (!this.tokenService.isValidToken(TokenType.Refresh)) {
+      this.router.navigate(['/login']);
+      return false;
+    }
+    return true;
+  }
+
+}
