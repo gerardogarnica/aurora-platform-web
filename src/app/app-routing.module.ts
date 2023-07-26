45955e494ee94008c2f75ffefca3ba23931e2521
gerardogarnica <gerardo.garnica@gmail.com>
2023-07-26 12:51:03 -0500

    Add guards to app routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from '@guards/auth.guard';
+import { RedirectGuard } from '@guards/redirect.guard';
 import { PageNotFoundComponent } from '@shared/pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
     path: '',
+    canActivate: [AuthGuard],
     loadChildren: () => import('@modules/main/main.module').then(m => m.MainModule)
   },
   {
     path: 'login',
+    canActivate: [RedirectGuard],
     loadChildren: () => import('@modules/auth/auth.module').then(m => m.AuthModule)
   },
   {
