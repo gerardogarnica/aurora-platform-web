9a8e1c57320e7dcbdb3398c7f399b3b6687bf550
gerardogarnica <gerardo.garnica@gmail.com>
2023-08-22 21:22:53 -0500

    Add module container shared component.

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,21 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+// Components
 import { MainButtonComponent } from './components/main-button/main-button.component';
+import { ModuleContainerComponent } from './components/module-container/module-container.component';
+
+// Pages
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     MainButtonComponent,
+    ModuleContainerComponent,
     PageNotFoundComponent
   ],
   imports: [
     CommonModule,
+    RouterModule,
     FontAwesomeModule
   ],
   exports: [
-    MainButtonComponent
+    MainButtonComponent,
+    ModuleContainerComponent
   ]
 })
 export class SharedModule { }
