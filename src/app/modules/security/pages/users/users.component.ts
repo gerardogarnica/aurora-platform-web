507bb3fe3196c0161ec73f09d1f4e85704dab815
gerardogarnica <gerardo.garnica@gmail.com>
2023-08-24 21:42:45 -0500

    Add pagination in the users page.

--- src/app/modules/security/pages/users/users.component.ts
+++ src/app/modules/security/pages/users/users.component.ts
@@ -1,29 +1,45 @@
 import { Component } from '@angular/core';
 
 import { User } from '@models/security/user.model';
-
+import { PagedCollections } from '@models/shared/paged-collections.model';
 import { UsersService } from '@services/users.service';
+import { PaginationRequest } from '@shared/components/pagination/pagination.component';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html'
 })
 export class UsersComponent {
-  users: User[] = [];
+  collection: PagedCollections<User> = {} as PagedCollections<User>;
+  pagination: PaginationRequest = { currentPage: 1, pageSize: 10 };
 
   constructor(
     private userService: UsersService
   ) { }
 
   ngOnInit(): void {
+    this.onPageChange();
+  }
+
+  onGoTo(page: number): void {
+    this.pagination.currentPage = page;
+    this.onPageChange();
+  }
+
+  onPageSizeChange(pageSize: number): void {
+    this.pagination.pageSize = pageSize;
+    this.pagination.currentPage = 1;
+    this.onPageChange();
+  }
+
+  onPageChange(): void {
     this.userService
-      .getUsers(0, 10)
+      .getUsers(this.pagination.currentPage - 1, this.pagination.pageSize)
       .subscribe({
         next: collection => {
-          console.log(collection.items);
-          this.users = collection.items;
+          console.log(collection);
+          this.collection = collection;
         }
       });
   }
-
 }
