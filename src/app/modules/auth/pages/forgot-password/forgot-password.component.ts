c3a951608c64a09a39d0de6ca97aadfd289d05b0
gerardogarnica <gerardo.garnica@gmail.com>
2023-09-07 12:26:56 -0500

    Add forgot password page.

---
+++ src/app/modules/auth/pages/forgot-password/forgot-password.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { Observable } from 'rxjs';
+
+import { ProcessStatus } from '@models/shared/process-status.model';
+import { AuthService } from '@services/auth.service';
+import { ErrorsService } from '@services/errors.service';
+
+@Component({
+  selector: 'app-forgot-password',
+  templateUrl: './forgot-password.component.html'
+})
+export class ForgotPasswordComponent {
+  faEnvelope = faEnvelope;
+
+  errorMessage$: Observable<string>;
+
+  form = this.formBuilder.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]]
+  });
+  status: ProcessStatus = 'init';
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthService,
+    private errorService: ErrorsService
+  ) {
+    this.errorMessage$ = this.errorService.errorMessage$;
+  }
+
+  doReset(event: Event) {
+    event.preventDefault();
+
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    this.status = 'loading';
+  }
+}
