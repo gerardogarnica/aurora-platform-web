e26aba6113f9a4c9fe32c123ff4e569acec0704f
gerardogarnica <gerardo.garnica@gmail.com>
2023-09-07 10:32:24 -0500

    Add tailwind custom colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,34 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        danger: colors.red,
-        primary: colors.blue,
-        success: colors.green
+        'theme': {
+          light: '#e4ebf5',
+          dark: '#cedeef'
+        },
+        'card': '#0f497d',
+        'label': {
+          DEFAULT: '#fafafa',
+          opaque: '#dcdcdc',
+          controls: '#000000'
+        },
+        'control': {
+          primary: '#f5f5f5',
+          DEFAULT: '#dcdcdc',
+          secondary: '#989898'
+        },
+        'danger': {
+          DEFAULT: '#f87171'
+        },
+        'highlight': {
+          primary: '#38bcf8',
+          DEFAULT: '#075e85',
+          secondary: '#bae8fd'
+        }
       },
       fontFamily: {
         roboto: ['Roboto', 'Helvetica Neue', 'sans-serif'],
         trebuchet: ['Trebuchet MS', 'Lucida Sans', 'sans-serif'],
-        montserrat: ['Montserrat', 'sans-serif'],
-        quicksand: ['Quicksand', 'sans-serif']
+        montserrat: ['Montserrat', 'sans-serif']
       }
     },
   },
